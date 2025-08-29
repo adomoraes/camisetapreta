6e189159da0d2adae4d1c304460076ba454aa083
Eduardo Moraes <adomoraes@gmail.com>
2025-08-29 11:10:14 -0300

    fix(ImageAttribution): translate 'Photo by' to 'Foto por'

--- src/components/ImageAttribution.tsx
+++ src/components/ImageAttribution.tsx
@@ -7,7 +7,7 @@ export function ImageAttribution({author}: Properties) {
 		<>
 			<div className='absolute top-0 h-full w-full bg-linear-to-b from-transparent via-transparent to-current text-black text-opacity-50' />
 			<div className='absolute right-1 bottom-1 px-1 text-sm text-white'>
-				Photo by {author} on Unsplash
+				Foto por {author} on Unsplash
 			</div>
 		</>
 	)
